64f35432d683edc2a0abb918c19fc321b7ac6a20
Jhant23 <jhant1@mail.ccsf.edu>
2022-06-11 20:34:58 -0500

    env puerto de base de datos agregada

--- db/connection.js
+++ db/connection.js
@@ -3,6 +3,7 @@ const { Sequelize } = require('sequelize');
 
 const db = new Sequelize(process.env.DATABASE,process.env.DBUSER,process.env.DBPASS, {
     host: process.env.DBHOST,
+    port: process.env.DBPORT,
     dialect: 'mysql'
 });
 
