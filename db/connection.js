3ae74101d8ddfb1bd75762678945a216dfd26ec3
Jhant23 <jhant1@mail.ccsf.edu>
2022-06-12 02:34:42 -0500

    Prueba producción

--- db/connection.js
+++ db/connection.js
@@ -4,7 +4,8 @@ const { Sequelize } = require('sequelize');
 const db = new Sequelize(process.env.DATABASE,process.env.DBUSER,process.env.DBPASS, {
     host: process.env.DBHOST,
     port: process.env.DBPORT,
-    dialect: 'mysql'
+    dialect: 'mysql',
+    logging: false
 });
 
 module.exports = db;
